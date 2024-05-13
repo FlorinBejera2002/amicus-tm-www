81fa3165000c3d75e706f8178321e4d9293d6316
FlorinBejera2002 <florinpetru0306@gmail.com>
2024-05-13 18:45:11 +0300

    fix: make design responsive for podcast page

--- src/app/[locale]/projects/podcast/bottom-section.tsx
+++ src/app/[locale]/projects/podcast/bottom-section.tsx
@@ -12,19 +12,17 @@ export default function BottomSection() {
 
   return (
     <div className="container flex flex-col justify-center items-center">
-      <h1 className="py-5 text-white font-bold text-3xl">
+      <h1 className="py-5 text-white font-bold text-2xl md:text-3xl">
         {t('bottom-title')}
       </h1>
       <iframe
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen={true}
-        className="pb-5"
-        height={550}
+        className="pb-5 md:w-[900px] md:h-[500px]"
         src="https://www.youtube.com/embed?listType=playlist&list=UUQy-aDJ2zjmVD7Yn2XusrsA&controls=0&modestbranding=1&index=1"
         title="YouTube Playlist"
-        width={900}
       />
-      <div className="grid grid-cols-2 justify-center items-center pt-10  border-t border-white">
+      <div className="grid md:grid-cols-2 justify-center items-center pt-10  border-t border-white">
         <Image alt="arise for christ logo" className="" src={mobileScreen} />
         <div className="flex flex-col justify-center items-center text-white">
           <h2 className="pb-4">{t('bottom-subtitle')}</h2>
