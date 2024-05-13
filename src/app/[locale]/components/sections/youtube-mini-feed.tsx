1d2134cd2f2bf8953a41158b84ea5b60e30aebe7
FlorinBejera2002 <florinpetru0306@gmail.com>
2024-05-13 18:25:34 +0300

    fIx: retouched youtube mini feed section

--- src/app/[locale]/components/sections/youtube-mini-feed.tsx
+++ src/app/[locale]/components/sections/youtube-mini-feed.tsx
@@ -2,14 +2,13 @@ export default function YoutubeMiniFeed() {
   return (
     <div className="lg:custom-grid-1 lg:grid grid-cols-1">
       <div
-        className="grid gap-1 lg:gap-2 md:grid-cols-2 lg:grid-cols-4 bg-transparent relative pt-1 lg:pt-2 xl:pt-3 pb-5 z-index-3 md:px-10 md:top-[-50px]"
+        className="grid gap-2 md:grid-cols-2 lg:grid-cols-4 bg-transparent relative pt-1 lg:pt-2 xl:pt-3 pb-5 z-index-3 md:px-24 md:top-[-50px]"
         id="numbers"
       >
         <iframe
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen={true}
-          className="w-full"
-          frameBorder="0"
+          className="w-full h-52 rounded-md"
           src="https://www.youtube.com/embed?listType=playlist&list=UUQy-aDJ2zjmVD7Yn2XusrsA&controls=0&modestbranding=1&index=1"
           title="YouTube Playlist"
         />
@@ -17,24 +16,21 @@ export default function YoutubeMiniFeed() {
         <iframe
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen={true}
-          className="w-full hidden md:flex"
-          frameBorder="0"
+          className="w-full hidden md:flex h-52 rounded-md"
           src="https://www.youtube.com/embed?listType=playlist&list=UUQy-aDJ2zjmVD7Yn2XusrsA&controls=0&modestbranding=1&index=2"
           title="YouTube Playlist"
         />
         <iframe
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen={true}
-          className="w-full hidden md:flex"
-          frameBorder="0"
+          className="w-full hidden md:flex h-52 rounded-md"
           src="https://www.youtube.com/embed?listType=playlist&list=UUQy-aDJ2zjmVD7Yn2XusrsA&controls=0&modestbranding=1&index=3"
           title="YouTube Playlist"
         />
         <iframe
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen={true}
-          className="w-full hidden md:flex"
-          frameBorder="0"
+          className="w-full hidden md:flex h-52 rounded-md"
           src="https://www.youtube.com/embed?listType=playlist&list=UUQy-aDJ2zjmVD7Yn2XusrsA&controls=0&modestbranding=1&index=4"
           title="YouTube Playlist"
         />
