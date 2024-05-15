dac9d2624650929eb95bb02d1fdcb1c586f11ef2
FlorinBejera2002 <florinpetru0306@gmail.com>
2024-05-15 14:33:07 +0300

    fix: finis design

--- src/app/[locale]/components/sections/evangelism-form.tsx
+++ src/app/[locale]/components/sections/evangelism-form.tsx
@@ -15,7 +15,7 @@ function Modal() {
     <>
       {modal && (
         <dialog className="md:fixed left-0 top-0 w-full h-full bg-black bg-opacity-50 z-50 my-24 md:my-0 p-4 overflow-auto backdrop-blur flex justify-center items-center">
-          <div className="bg-gradient-to-t relative bg-white rounded-md md:w-[50em] lg:w-[65em] mt-4 flex justify-center items-center">
+          <div className="bg-gradient-to-t relative bg-white rounded-md md:w-[50em] lg:w-[65em] mt-24 flex justify-center items-center">
             <Link
               className="flex justify-end absolute right-12 top-12 cursor-pointer"
               href={pathname}
