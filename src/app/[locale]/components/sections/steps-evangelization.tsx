55d5ede02b38b8ef84a54eb6bf25f184c041ea6d
FlorinBejera2002 <florinpetru0306@gmail.com>
2024-05-16 13:35:38 +0300

    fix: fixed structure for steps evangelization

--- src/app/[locale]/components/sections/steps-evangelization.tsx
+++ src/app/[locale]/components/sections/steps-evangelization.tsx
@@ -3,11 +3,14 @@ import { StepEvangelization } from './step-evangelization'
 import { PiNumberCircleOneFill } from 'react-icons/pi'
 import { PiNumberCircleTwoFill } from 'react-icons/pi'
 import { PiNumberCircleThreeFill } from 'react-icons/pi'
+import { useTranslations } from 'next-intl'
 import Image from 'next/image'
 
 import logoArise from '../../../../../public/logo_horizontal_black.png'
 
 export default function StepsEvangelization() {
+  const t = useTranslations('steps_evanghelization')
+
   return (
     <section id="concept">
       <div className="md:mx-48 d-flex justify-center gap-16  bg-color-light p-relative z-index-2 pb-10">
@@ -21,25 +24,22 @@ export default function StepsEvangelization() {
         </div>
         <div className="flex flex-col gap-8 md:gap-12 xl:gap px-2 md:px-4">
           <h1 className="text-3xl md:text-4xl xl:text-5xl font-poppins text-center ">
-            <b>Pasii de evanghelizare</b>
+            {t('title')}
           </h1>
           <StepEvangelization
-            description="Intră pe site-ul nostru și caută butonul cu cererea de
-                evanghelizare."
-            header="Scanează codul QR"
+            description={t('step1_description')}
+            header={t('step1')}
             numberImage={<PiNumberCircleOneFill className="w-24 h-24" />}
           />
           <StepEvangelization
-            description=" Completeaza o cerere de evanghelizare!
-            "
-            header="Completează cererea"
+            description={t('step2_description')}
+            header={t('step2')}
             numberImage={<PiNumberCircleTwoFill className="w-24 h-24" />}
             styles="xl:justify-center"
           />
           <StepEvangelization
-            description=" Noi vom prelua această cerere pentru a face un plan de
-            evanghelizare!"
-            header="Noi îl vom contacta"
+            description={t('step3_description')}
+            header={t('step2')}
             numberImage={<PiNumberCircleThreeFill className="w-24 h-24" />}
             styles="xl:justify-center"
           />
