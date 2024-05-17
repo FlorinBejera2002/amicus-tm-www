a679866f8e793e80001d21820208561c8ae3675c
ElvisMuresan <elvis.e.muresan@gmail.com>
2024-05-17 23:46:29 +0300

    fix: Fixed the Scroll button

--- src/app/[locale]/components/sections/scroll-button.tsx
+++ src/app/[locale]/components/sections/scroll-button.tsx
@@ -2,19 +2,17 @@
 import React, { useState } from 'react'
 
 import { FaChevronUp } from 'react-icons/fa'
+import { useWindowScroll } from '@uidotdev/usehooks'
 
 export default function ScrollButton() {
   const [visible, setVisible] = useState(false)
+  const [{ y }] = useWindowScroll()
 
-  const toggleVisible = () => {
-    const scrolled = document.documentElement.scrollTop
-
-    if (scrolled > 300) {
-      setVisible(true)
-    } else if (scrolled <= 300) {
-      setVisible(false)
+  React.useEffect(() => {
+    if (y !== null) {
+      setVisible(y > 300)
     }
-  }
+  }, [y])
 
   const scrollToTop = () => {
     window.scrollTo({
@@ -23,11 +21,14 @@ export default function ScrollButton() {
     })
   }
 
-  window.addEventListener('scroll', toggleVisible)
-
   return (
-    <button className=" fixed bottom-20 right-2.5 flex justify-center items-center text-center bg-[#e3ae04] hidden-mobile visible p-3 min-w-12 text-lg opacity-75 transition-opacity duration-300 z-40">
-      <FaChevronUp onClick={scrollToTop} />
-    </button>
+    visible && (
+      <button
+        className="fixed bottom-20 right-2.5 flex justify-center items-center text-center bg-[#e3ae04] rounded-md hidden-mobile visible p-3 min-w-12 text-lg opacity-75 transition-opacity duration-300 z-40"
+        onClick={scrollToTop}
+      >
+        <FaChevronUp />
+      </button>
+    )
   )
 }
