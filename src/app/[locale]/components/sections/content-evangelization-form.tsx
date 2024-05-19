4fbaee0447e7ce25cdbb14b6256756509cd0cdf6
Vlad Crișan <vlad.crishan20@gmail.com>
2024-05-19 14:53:54 +0300

    feat: Submitting form data to backend

--- src/app/[locale]/components/sections/content-evangelization-form.tsx
+++ src/app/[locale]/components/sections/content-evangelization-form.tsx
@@ -12,6 +12,7 @@ import { cn } from '@/utils'
 
 import horizontalLogo from '../../../../../public/logo_horizontal_black.png'
 
+const CMS_URL = 'https://cms-backend.ariseforchrist.com'
 const MAILCHIMP_URL =
   'https://app.us22.list-manage.com/subscribe/post?u=41ac89857e074d884af936f25&amp;id=cc995c9835&amp;f_id=00c8c4e1f0'
 
@@ -37,19 +38,40 @@ const ContentEvangelizationForm = () => {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const handleSubmit = async (event: any) => {
           event.preventDefault()
+          const name = event.target.name.value
           const email = event.target.email.value
+          const phone = event.target.phone.value
+          const details = event.target.details.value
 
+          // submit
           try {
+            await fetch(CMS_URL + '/evangelism-requests', {
+              body: JSON.stringify({
+                applicantEmail: email,
+                applicantMobile: phone,
+                applicantName: name,
+                category: 'EvangelismRequest',
+                otherdetails: details,
+                status: 'InProgress'
+              }),
+              headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+              },
+              method: 'POST'
+            })
+
             if (isChecked) {
               await subscribe({ EMAIL: email })
-              setMessage({
-                content: t('successful_submission'),
-                type: 'success'
-              })
-              setTimeout(() => {
-                router.push('/')
-              }, 3000)
             }
+
+            setMessage({
+              content: t('successful_submission'),
+              type: 'success'
+            })
+            setTimeout(() => {
+              router.push('/')
+            }, 3000)
           } catch (error) {
             setMessage({
               content: 'A apărut o eroare la trimiterea formularului: ' + error,
@@ -128,7 +150,6 @@ const ContentEvangelizationForm = () => {
                     className="block px-0 w-full p-1 text-sm text-gray-900 bg-transparent border-b border-gray-500 appearance-none focus:outline-none focus:ring-0 focus:border-yellow-500 peer"
                     id="phone"
                     name="phone"
-                    required={true}
                     type="phone"
                   />
                   <label
@@ -144,7 +165,7 @@ const ContentEvangelizationForm = () => {
                 <textarea
                   className="resize-none block px-0 w-full p-1 text-sm text-gray-900 bg-transparent border-b border-gray-500 appearance-none focus:outline-none focus:ring-0 focus:border-yellow-500 peer"
                   id="details"
-                  name="det"
+                  name="details"
                   rows={1}
                 />
                 <label
@@ -182,7 +203,6 @@ const ContentEvangelizationForm = () => {
               <div className="flex justify-center items-center pb-3 pt-3 md:py-0">
                 <button
                   className="bg-[#e3ae04] rounded-md px-20 hover:shadow-2xl hover:-translate-2 hover:scale-105 hover:bg-[#e3ae04] duration-700 text-black font-bold py-1 size-9 flex justify-center items-center disabled:opacity-50 disabled:pointer-events-none"
-                  disabled={!isChecked}
                   type="submit"
                 >
                   {t('send')}
