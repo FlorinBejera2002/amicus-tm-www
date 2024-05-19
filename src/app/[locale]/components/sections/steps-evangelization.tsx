31489ab72f6df2e597563049a0eed406ded9a40d
Vlad Crișan <vlad.crishan20@gmail.com>
2024-05-19 14:35:29 +0300

    feat: Adding fixed EV count (temp)

--- src/app/[locale]/components/sections/steps-evangelization.tsx
+++ src/app/[locale]/components/sections/steps-evangelization.tsx
@@ -4,34 +4,34 @@ import { PiNumberCircleOneFill } from 'react-icons/pi'
 import { PiNumberCircleTwoFill } from 'react-icons/pi'
 import { PiNumberCircleThreeFill } from 'react-icons/pi'
 import { useTranslations } from 'next-intl'
-import Image from 'next/image'
-
-import stepsImage from '../../../../../public/steps-image.jpg'
 
 export default function StepsEvangelization() {
   const t = useTranslations('steps_evanghelization')
 
   return (
     <section id="concept">
-      <div className="md:mx-48 grid md:grid-cols-2  bg-color-light p-relative z-index-2 pb-10">
-        <div className="hidden md:flex items-center justify-center">
-          <Image
-            alt="Stps for Evangelisation"
-            className="rounded-tr-[10em]"
-            height={400}
-            src={stepsImage}
-            width={400}
-          />
+      <div className="grid bg-color-light p-relative z-index-2 pb-10 md:mx-20 xl:mx-48 xl:grid-cols-2">
+        <div className="flex mb-20 xl:flex items-center justify-center">
+          <div className="flex flex-col w-1/2 bg-black p-5 rounded-tr-[7rem]">
+            <h4 className="text-5xl font-weight-semi-bold text-light mb-4">
+              585+
+            </h4>
+
+            <div className="text-md text-white">
+              {t('evangelism_requests_count')}
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col gap-8 md:gap-12 xl:gap-16 px-2 md:px-4">
-          <h1 className="text-3xl md:text-4xl xl:text-5xl font-poppins font-bold ">
+
+        <div className="flex flex-col gap-8 px-2 md:px-4 md:gap-12 xl:gap-16">
+          <h1 className="text-3xl font-poppins font-bold text-center md:text-4xl xl:text-5xl">
             {t('title')}
           </h1>
           <StepEvangelization
             description={t('step1_description')}
             header={t('step1')}
             numberImage={<PiNumberCircleOneFill className="w-24 h-24" />}
-            styles="xl:justify-start"
+            styles="m xl:justify-start"
           />
           <StepEvangelization
             description={t('step2_description')}
