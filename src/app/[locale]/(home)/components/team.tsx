23bb99a5cfa1834d131c56d57c72f4ca50dac31f
FlorinBejera2002 <florinpetru0306@gmail.com>
2024-05-24 01:21:56 +0300

    delete: delete link with the cards team

--- src/app/[locale]/(home)/components/team.tsx
+++ src/app/[locale]/(home)/components/team.tsx
@@ -1,7 +1,6 @@
 import InViewTransition from '../../common/in-view-transition'
 
 import { useTranslations } from 'next-intl'
-import Link from 'next/link'
 import Image from 'next/image'
 
 import Simona from '../../../../../public/img/team/simo.webp'
@@ -16,7 +15,7 @@ export default function Team() {
 
   return (
     <div className="bg-color-dark p-relative custom-section-1 bg-position-center bg-size-cover lazyload px-8">
-      <div className="md:mx-48 px-2 py-4">
+      <div className="container px-2 py-4">
         <div className="row align-items-center ">
           <b className="text-3xl text-gray-50 font-bold py-6 text-center">
             {t('title-a4c')}
@@ -43,15 +42,13 @@ export default function Team() {
 
             <InViewTransition damping={50} delay={0.75}>
               <div className="mt-3 bg-gradient-to-t from-gray-800 to-transparent rounded-xl gap-1 flex flex-col justify-center  items-center  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-900 duration-300 ">
-                <Link href="https://www.youtube.com/watch?v=XhQIeYzccUI">
-                  <Image
-                    alt="Dana Potra"
-                    className="border-b border-gray-600 py-3"
-                    height={150}
-                    src={Dana}
-                    width={150}
-                  />
-                </Link>
+                <Image
+                  alt="Dana Potra"
+                  className="border-b border-gray-600 py-3"
+                  height={150}
+                  src={Dana}
+                  width={150}
+                />
                 <div className="flex flex-col justify-center items-center h-24 px-3 ">
                   <b className="text-gray-50">Dana Potra</b>
                   <p className="text-gray-500 text-sm">{t('dana-potra')}</p>
@@ -60,15 +57,13 @@ export default function Team() {
             </InViewTransition>
             <InViewTransition damping={50} delay={1}>
               <div className="mt-3 bg-gradient-to-t from-gray-800 to-transparent rounded-xl gap-1 flex flex-col justify-center  items-center  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-900 duration-300 ">
-                <Link href="https://www.youtube.com/watch?v=XhQIeYzccUI">
-                  <Image
-                    alt="Avram Sabou"
-                    className="border-b border-gray-600 py-3"
-                    height={150}
-                    src={AvramSabou}
-                    width={150}
-                  />
-                </Link>
+                <Image
+                  alt="Avram Sabou"
+                  className="border-b border-gray-600 py-3"
+                  height={150}
+                  src={AvramSabou}
+                  width={150}
+                />
                 <div className="flex flex-col justify-center items-center h-24 px-3 xl:pt-3">
                   <b className="text-gray-50">Avram Sabou</b>
                   <p className="text-gray-500 text-sm text-center">
@@ -79,15 +74,13 @@ export default function Team() {
             </InViewTransition>
             <InViewTransition damping={50} delay={1.25}>
               <div className="mt-3 bg-gradient-to-t from-gray-800 to-transparent rounded-xl gap-1 flex flex-col justify-center  items-center  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-900 duration-300 ">
-                <Link href="https://www.youtube.com/watch?v=XhQIeYzccUI">
-                  <Image
-                    alt="Adi Kovaci"
-                    className="border-b border-gray-600 py-3"
-                    height={150}
-                    src={AdrianCovaci}
-                    width={150}
-                  />
-                </Link>
+                <Image
+                  alt="Adi Kovaci"
+                  className="border-b border-gray-600 py-3"
+                  height={150}
+                  src={AdrianCovaci}
+                  width={150}
+                />
                 <div className="flex flex-col justify-center items-center h-24 px-3 ">
                   <b className="text-gray-50">Adi Kovaci</b>
                   <p className="text-gray-500 text-sm">{t('adi-kovaci')}</p>
@@ -96,15 +89,13 @@ export default function Team() {
             </InViewTransition>
             <InViewTransition damping={50} delay={1.5}>
               <div className="mt-3 bg-gradient-to-t from-gray-800 to-transparent rounded-xl gap-1 flex flex-col justify-center  items-center  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-900 duration-300 ">
-                <Link href="https://www.youtube.com/watch?v=XhQIeYzccUI">
-                  <Image
-                    alt="Andrei Birtea"
-                    className="border-b border-gray-600 py-3"
-                    height={150}
-                    src={Andrei}
-                    width={150}
-                  />
-                </Link>
+                <Image
+                  alt="Andrei Birtea"
+                  className="border-b border-gray-600 py-3"
+                  height={150}
+                  src={Andrei}
+                  width={150}
+                />
                 <div className="flex flex-col justify-center items-center h-24 px-3 ">
                   <b className="text-gray-50">Andrei Birtea</b>
                   <p className="text-gray-500 text-sm ">{t('andrei-birtea')}</p>
@@ -113,15 +104,13 @@ export default function Team() {
             </InViewTransition>
             <InViewTransition damping={50} delay={1.75}>
               <div className="mt-3 bg-gradient-to-t from-gray-800 to-transparent rounded-xl gap-1 flex flex-col justify-center  items-center  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-900 duration-300 ">
-                <Link href="https://www.youtube.com/watch?v=XhQIeYzccUI">
-                  <Image
-                    alt="Simona Birtea"
-                    className="border-b border-gray-600 py-3"
-                    height={150}
-                    src={Simona}
-                    width={150}
-                  />
-                </Link>
+                <Image
+                  alt="Simona Birtea"
+                  className="border-b border-gray-600 py-3"
+                  height={150}
+                  src={Simona}
+                  width={150}
+                />
                 <div className="flex flex-col justify-center items-center h-24 px-3">
                   <b className="text-gray-50">Simona Birtea </b>
                   <p className="text-gray-500 text-sm">{t('simona-birtea')}</p>
