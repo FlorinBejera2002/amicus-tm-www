43ebb12a39f056878b5483e7932709f41c0e9012
Vlad Crișan <vlad.crishan20@gmail.com>
2024-07-17 18:22:32 +0300

    fix: Fix env variable

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+    SUPABASE_URL: process.env.SUPABASE_URL
+  }
+}
 
 export default withNextIntl(nextConfig)
