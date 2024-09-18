85850603e9e4fcf22d5d27d3fa6858c2cf1495f6
Vlad Crișan <vlad.crishan20@gmail.com>
2024-09-19 01:18:19 +0300

    feat: Updating donate 3.5% section

--- src/app/[locale]/donate/page.tsx
+++ src/app/[locale]/donate/page.tsx
@@ -1,41 +1,35 @@
-import Animation from '../common/animation'
-import CustomHeroBg from '../common/custom-hero-bg'
-import InViewTransition from '../common/in-view-transition'
+import Animation from "../common/animation";
+import CustomHeroBg from "../common/custom-hero-bg";
+import InViewTransition from "../common/in-view-transition";
 
-import { useTranslations } from 'next-intl'
+import { useTranslations } from "next-intl";
 
-import { Metadata } from 'next'
+import { Metadata } from "next";
 
-import Image from 'next/image'
-import Link from 'next/link'
-import { FaDonate } from 'react-icons/fa'
-import { FaAngleRight } from 'react-icons/fa'
+import Image from "next/image";
+import Link from "next/link";
+import { FaDonate } from "react-icons/fa";
+import { FaFileAlt } from "react-icons/fa";
+import { FaAngleRight } from "react-icons/fa";
 
-import CECLogo from '../../../../public/CEC Bank.svg'
-import PayPal from '../../../../public/PayPal.png'
-import QRPayPal from '../../../../public/QRPayPal.png'
-import RevolutLogo from '../../../../public/Revolut-Logo.png'
-import QRStripe from '../../../../public/qr-stripe.png'
-import StripeLogo from '../../../../public/srtripe.png'
+import CECLogo from "../../../../public/CEC Bank.svg";
+import PayPal from "../../../../public/PayPal.png";
+import QRPayPal from "../../../../public/QRPayPal.png";
+import RevolutLogo from "../../../../public/Revolut-Logo.png";
+import QRStripe from "../../../../public/qr-stripe.png";
+import StripeLogo from "../../../../public/srtripe.png";
 
 export const metadata: Metadata = {
-  title: 'Arise for Christ | Donate'
-}
+  title: "Arise for Christ | Donate",
+};
 
 const About = () => {
-  const t = useTranslations()
- 
+  const t = useTranslations();
 
   return (
     <div className="main" role="main">
       <section className="page-header p-relative bg-quaternary m-0 overflow-hidden border-0 p-0 ">
-        <InViewTransition
-          damping={100}
-          delay={0.25}
-          duration={0.5}
-          yIn={0}
-          yOut={0}
-        >
+        <InViewTransition damping={100} delay={0.25} duration={0.5} yIn={0} yOut={0}>
           <CustomHeroBg />
         </InViewTransition>
         <div className="row align-items-center !my-[31px] pb-4 pt-5">
@@ -44,20 +38,15 @@ const About = () => {
               <div className="py-4">
                 <ul className="breadcrumb justify-content-center text-4-5 font-weight-medium mb-2 flex gap-2">
                   <li className="z-[1]">
-                    <Link
-                      className="text-color-primary text-decoration-none"
-                      href="/"
-                    >
-                      {t('header.home')}
+                    <Link className="text-color-primary text-decoration-none" href="/">
+                      {t("header.home")}
                     </Link>
                   </li>
                   <FaAngleRight className="text-color-primary z-[1] mt-0.5" />
-                  <li className="text-color-primary active z-[1]">
-                    {t('header.donate')}
-                  </li>
+                  <li className="text-color-primary active z-[1]">{t("header.donate")}</li>
                 </ul>
                 <h1 className="text-color-light font-weight-bold text-4xl md:text-5xl">
-                  {t('header.donate')}
+                  {t("header.donate")}
                 </h1>
               </div>
             </InViewTransition>
@@ -69,53 +58,37 @@ const About = () => {
 
       <div className="custom-page-content p-relative z-index-2 text-md flex w-full justify-center ">
         <div className="max-w-6xl flex flex-col content-center rounded-md bg-white p-8 shadow-lg md:p-12 gap-10">
-
           <div className="max-w-full">
             <InViewTransition delay={0.25}>
               <h1 className="text-2xl md:text-3xl font-bold text-gray-800 mb-3">
-                {t('donate.title_2')}
+                {t("donate.title_2")}
               </h1>
 
               <div className="grid grid-cols-1 gap-8 md:grid-cols-2 md:flex-row xl:grid-cols-4">
                 <InViewTransition delay={0.25}>
                   <div className=" flex w-full flex-col items-center justify-between gap-10 rounded-lg bg-gray-50 p-10 shadow-md md:h-96">
-                    <Image
-                      alt="Logo for the bank"
-                      className="w-32"
-                      src={RevolutLogo}
-                    />
+                    <Image alt="Logo for the bank" className="w-32" src={RevolutLogo} />
                     <div className="flex flex-col items-center justify-center text-center">
                       <div>
-                        <p className="mb-1 font-semibold text-gray-700">
-                          {t('donate.phone')}
-                        </p>
+                        <p className="mb-1 font-semibold text-gray-700">{t("donate.phone")}</p>
                         <p className="text-gray-600">+40 773 307 292</p>
                       </div>
 
-                      <p className="font-semibold text-gray-700">
-                        @ariseforchrist
-                      </p>
+                      <p className="font-semibold text-gray-700">@ariseforchrist</p>
                       <Link
                         className="!no-underline"
                         href="https://business.revolut.com/overview"
                         target="_blank"
                       >
-                        <Image
-                          alt="QR Code for donation"
-                          className="w-28"
-                          src={QRStripe}
-                        />
+                        <Image alt="QR Code for donation" className="w-28" src={QRStripe} />
                       </Link>
                     </div>
                   </div>
                 </InViewTransition>
+
                 <InViewTransition delay={0.25}>
                   <div className="flex flex-col items-center justify-between gap-16 rounded-lg bg-purple-50 p-10 shadow-md md:h-96">
-                    <Image
-                      alt="Logo for the bank"
-                      className="w-24"
-                      src={StripeLogo}
-                    />
+                    <Image alt="Logo for the bank" className="w-24" src={StripeLogo} />
                     <div className="flex flex-col items-center justify-center gap-10 text-center">
                       <div className="flex flex-col items-center justify-center">
                         <Link
@@ -125,7 +98,7 @@ const About = () => {
                         >
                           <span className="flex cursor-pointer items-center gap-2 rounded-md bg-accent px-3 py-2 text-center font-semibold text-black">
                             <FaDonate />
-                            {t('donate.donate')}
+                            {t("donate.donate")}
                           </span>
                         </Link>
                       </div>
@@ -134,11 +107,7 @@ const About = () => {
                         href="https://buy.stripe.com/dR66oueifggya3ubII"
                         target="_blank"
                       >
-                        <Image
-                          alt="QR Code for donation"
-                          className="w-28"
-                          src={QRStripe}
-                        />
+                        <Image alt="QR Code for donation" className="w-28" src={QRStripe} />
                       </Link>
                     </div>
                   </div>
@@ -146,11 +115,7 @@ const About = () => {
 
                 <InViewTransition delay={0.25}>
                   <div className=" flex w-full flex-col items-center justify-between gap-16 rounded-lg bg-blue-50 p-10 shadow-md md:h-96">
-                    <Image
-                      alt="Logo for the bank"
-                      className="w-36"
-                      src={PayPal}
-                    />
+                    <Image alt="Logo for the bank" className="w-36" src={PayPal} />
                     <div className="flex flex-col items-center justify-center gap-10 text-center">
                       <div className="flex flex-col items-center justify-center">
                         <Link
@@ -160,7 +125,7 @@ const About = () => {
                         >
                           <span className="flex cursor-pointer items-center gap-2 rounded-md bg-accent px-3 py-2 text-center font-semibold text-black">
                             <FaDonate />
-                            {t('donate.donate')}
+                            {t("donate.donate")}
                           </span>
                         </Link>
                       </div>
@@ -169,46 +134,29 @@ const About = () => {
                         href="https://www.paypal.com/donate/?hosted_button_id=D8M92EYVFPSM6"
                         target="_blank"
                       >
-                        <Image
-                          alt="QR Code for donation"
-                          className="w-[104px]"
-                          src={QRPayPal}
-                        />
+                        <Image alt="QR Code for donation" className="w-[104px]" src={QRPayPal} />
                       </Link>
                     </div>
                   </div>
                 </InViewTransition>
+
                 <InViewTransition delay={0.25}>
                   <div className="flex flex-col items-center justify-between gap-6 rounded-lg bg-green-50 p-10 shadow-md md:h-96 lg:w-full">
-                    <Image
-                      alt="Logo for the bank"
-                      className="w-48"
-                      src={CECLogo}
-                    />
+                    <Image alt="Logo for the bank" className="w-48" src={CECLogo} />
                     <div className="flex flex-col text-center gap-4">
                       <div className="flex flex-col gap-1">
                         <span className="font-semibold text-gray-700">
-                          {t('donate.beneficiary')}
+                          {t("donate.beneficiary")}
                         </span>
-                        <span className="text-gray-600">
-                          {t('donate.name')}
-                        </span>
+                        <span className="text-gray-600">{t("donate.name")}</span>
                       </div>
                       <div className="flex flex-col gap-1">
-                        <span className="font-semibold text-gray-700">
-                          {t('donate.ron')}
-                        </span>
-                        <span className="text-xs text-gray-600">
-                          RO36CECETM0130RON0998570
-                        </span>
+                        <span className="font-semibold text-gray-700">{t("donate.ron")}</span>
+                        <span className="text-xs text-gray-600">RO36CECETM0130RON0998570</span>
                       </div>
                       <div className="flex flex-col gap-1">
-                        <span className="font-semibold text-gray-700">
-                          {t('donate.dollars')}
-                        </span>
-                        <span className="text-xs text-gray-600">
-                          RO37CECETM01B8USD0998572
-                        </span>
+                        <span className="font-semibold text-gray-700">{t("donate.dollars")}</span>
+                        <span className="text-xs text-gray-600">RO37CECETM01B8USD0998572</span>
                       </div>
                     </div>
                   </div>
@@ -217,18 +165,26 @@ const About = () => {
             </InViewTransition>
           </div>
 
-          <InViewTransition delay={0.25}>
-            <h1 className="mb-3 text-2xl md:text-3xl font-bold" key={1}>
-              {t('donate.title_3')}
-            </h1>
-            <div className="w-full md:w-2/3 ">
-              <p className="mb-0">{t('donate.paragraf_5')}</p>
-            </div>
-          </InViewTransition>
+          <div className="mt-20">
+            <InViewTransition delay={0.25}>
+              <h1 className="mb-3 text-2xl md:text-3xl font-bold" key={1}>
+                {t("donate.title_3")}
+              </h1>
+
+              <span className="inline-flex cursor-pointer items-center gap-2 rounded-md bg-accent px-3 py-2 text-center font-semibold text-black opacity-50">
+                <FaFileAlt />
+                {t("donate.form")}
+              </span>
+
+              <div className="w-full mt-4">
+                <p className="mb-0">{t("donate.paragraf_5")}</p>
+              </div>
+            </InViewTransition>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
