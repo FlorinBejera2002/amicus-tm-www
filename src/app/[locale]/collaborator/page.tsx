dc97cce389f5a7949c43f95ccdf6b8911142ca0e
FlorinBejera2002 <florinpetru0306@gmail.com>
2024-09-30 09:33:54 +0300

    feat: add page for collaborators

--- src/app/[locale]/collaborator/page.tsx
+++ src/app/[locale]/collaborator/page.tsx
@@ -1,13 +1,13 @@
-import { Metadata } from 'next'
+import Animation from '../common/animation'
+import CustomHeroBg from '../common/custom-hero-bg'
+import InViewTransition from '../common/in-view-transition'
+import Collaborator from '../../../../public/collaborator.webp'
 import { useTranslations } from 'next-intl'
 import Image from 'next/image'
 import Link from 'next/link'
 import { FaAngleRight } from 'react-icons/fa'
-import Collaborator from '../../../../public/collaborator.webp'
-import Animation from '../common/animation'
-import CustomHeroBg from '../common/custom-hero-bg'
-import InViewTransition from '../common/in-view-transition'
 import CollaboratorForm from './collaborator-form'
+import { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Arise for Christ | Podcast'
