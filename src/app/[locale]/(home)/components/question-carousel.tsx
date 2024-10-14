a17b3f2715ea89ec6de69a251691df834ba74fbb
FlorinBejera2002 <florinpetru0306@gmail.com>
2024-10-14 12:40:54 +0300

    feat: inprove question on the hero section

--- src/app/[locale]/(home)/components/question-carousel.tsx
+++ src/app/[locale]/(home)/components/question-carousel.tsx
@@ -8,11 +8,17 @@ export const QuestionCarousel = () => {
 
   const questions = [t('question1'), t('question2'), t('question3')]
   const [currentIndex, setCurrentIndex] = useState(0)
+  const [isAnimating, setIsAnimating] = useState(true)
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % questions.length)
+      setIsAnimating(false)
+      setTimeout(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % questions.length)
+        setIsAnimating(true)
+      }, 800)
     }, 5000)
+
     return () => clearInterval(interval)
   }, [questions.length])
 
@@ -21,9 +27,17 @@ export const QuestionCarousel = () => {
       <h5 className="font-bold text-gray-500 text-sm md:text-xl">
         Ți-ai pus vreodată întrebarea:
       </h5>
-      <h3 className="text-2xl font-bold text-white md:text-5xl lg:text-5xl transition-opacity duration-1000 ease-in-out">
-        {questions[currentIndex]}
-      </h3>
+      <div className={`overflow-hidden h-20`}>
+        <div
+          className={`transform transition-transform duration-1000 ease-out ${
+            isAnimating ? 'translate-y-0' : 'translate-y-24 '
+          }`}
+        >
+          <h3 className="text-4xl font-bold text-white md:text-4xl lg:text-5xl">
+            {questions[currentIndex]}
+          </h3>
+        </div>
+      </div>
     </div>
   )
 }
