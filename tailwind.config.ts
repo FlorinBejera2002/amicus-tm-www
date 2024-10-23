821255e72451d496144af385c9913d5243f3c125
Vlad Crișan <vlad.crishan20@gmail.com>
2024-10-23 18:36:45 +0300

    fix: Adding change for deployment to vercel

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,8 +18,7 @@ const config: Config = {
         'outline-red': '0 0 10px 5px rgba(255, 0, 0, 0.5)'
       },
       colors: {
-        accent: '#e3ae00',
-        black: '#000'
+        accent: '#e3ae00'
       }
     }
   }
