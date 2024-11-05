77c42bf2c222e39c1cd152976632448c46f78039
FlorinBejera2002 <florinpetru0306@gmail.com>
2024-11-05 12:23:22 +0200

    feat: add secction about resurse crestine

---
+++ src/app/[locale]/(home)/components/about-resurse-crestine.tsx
@@ -0,0 +1,58 @@
+import InViewTransition from '@/app/[locale]/common/in-view-transition'
+import Image from 'next/image'
+import resurseCrestineLogo from '../../../../../public/resurse-crestine-image.png'
+
+import Link from 'next/link'
+import { useTranslations } from 'next-intl'
+
+export default function AboutResusrseCrestine() {
+  const t = useTranslations('')
+
+  return (
+    <div className="relative z-10 mx-auto  max-w-6xl grid md:grid-cols-2 justify-center rounded-lg bg-white p-8 md:p-12 shadow-lg md:-mt-16 xl:gap-20">
+      <InViewTransition
+        damping={25}
+        xOut={-100}
+        yOut={0}
+        customClassname=" rounded-md flex justify-center items-center"
+      >
+        <Link
+          href="https://www.resursecrestine.ro"
+          className="no-underline hover:no-underline !p-0"
+        >
+          <Image
+            alt="Logo Resurse Creștine"
+            className="object-cover hidden md:flex"
+            src={resurseCrestineLogo}
+          />
+        </Link>
+      </InViewTransition>
+      <div className="flex flex-col justify-center md:pr-10 lg:pr-16">
+        <InViewTransition delay={0.25}>
+          <h3 className="text-xl font-bold text-gray-900 md:pb-6 pb-3 md:text-3xl">
+            {t('resurse-crestine.asociatia_resurse_crestine')}
+          </h3>
+        </InViewTransition>
+
+        <InViewTransition delay={0.25}>
+          <p className="text-sm font-normal font-poppins text-gray-700 md:pb-4 md:text-base">
+            {t('resurse-crestine.descriere_asociatie')}
+            <Link href="https://www.resursecrestine.ro/" className="px-1">
+              www.resursecrestine.ro
+            </Link>
+            {t('resurse-crestine.materiale_gratuite')}
+          </p>
+        </InViewTransition>
+
+        <InViewTransition delay={0.25}>
+          <Link
+            href="https://www.resursecrestine.ro/"
+            className="bg-accent p-2.5 rounded-md no-underline hover:!no-underline text-black font-medium text-xs"
+          >
+            {t('resurse-crestine.vezi_mai_multe')}
+          </Link>
+        </InViewTransition>
+      </div>
+    </div>
+  )
+}
