a4c2d0196e4d804cd821c8a2873263498db034fb
Vlad Crișan <vlad.crishan20@gmail.com>
2024-11-23 17:58:53 +0100

    fix: linting

--- src/app/[locale]/collaborator/collaborator-button.tsx
+++ src/app/[locale]/collaborator/collaborator-button.tsx
@@ -6,9 +6,10 @@ import { FaHandsHelping } from 'react-icons/fa'
 export const CollaboratorButton = () => {
   const t = useTranslations()
   const handleMailToClick = (e: { preventDefault: () => void }) => {
-    e.preventDefault();
-    window.location.href = 'mailto:info@ariseforchrist.com?subject=Vreau%20să%20mă%20implic&body=Mă%20numesc%20...%20și%20vreau%20să%20mă%20implic%20în%20lucrare';
-  };
+    e.preventDefault()
+    window.location.href =
+      'mailto:info@ariseforchrist.com?subject=Vreau%20să%20mă%20implic&body=Mă%20numesc%20...%20și%20vreau%20să%20mă%20implic%20în%20lucrare'
+  }
 
   return (
     <button
