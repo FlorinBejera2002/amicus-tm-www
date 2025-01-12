d01f91e7c56d1e9cc0f11fc8c591cccdd68dfbc5
Vlad Crișan <vlad.crishan20@gmail.com>
2025-01-12 21:04:24 +0200

    feat: Improving mobile header by adding logo + adding vertical icon to resources

--- src/app/[locale]/(home)/components/card-collaborator.tsx
+++ src/app/[locale]/(home)/components/card-collaborator.tsx
@@ -11,13 +11,25 @@ export const CardCollaborator = () => {
   const language = pathname.split('/')[1]
 
   return (
-    <Link
-      href={`${language}/collaborator`}
-      className="flex flex-col w-fit items-center gap-4 rounded-md bg-accent px-3 py-2.5 text-xs font-semibold text-black no-underline hover:!no-underline"
-    >
-      <div className="flex gap-2 justify-between w-full items-center">
-        <p className="text-black text-bold m-0">{t('collaborator.involve')}</p>
-      </div>
-    </Link>
+    <div className='flex flex-col gap-3'>
+      <Link
+        href={`${language}/collaborator`}
+        className="flex flex-col w-fit items-center gap-4 rounded-md bg-accent px-3 py-2.5 text-xs font-semibold text-black no-underline hover:!no-underline"
+      >
+        <div className="flex gap-2 justify-between w-full items-center">
+          <p className="text-black text-bold m-0">
+            {t('collaborator.involve')}
+          </p>
+        </div>
+      </Link>
+
+      <Link
+        className="mr-auto flex rounded-md bg-accent px-3 py-2.5 text-xs font-semibold text-black no-underline hover:!no-underline lg:hidden w-auto"
+        href={`/${language}/evangelism-request`}
+        type="button"
+      >
+        {t('form.title')}
+      </Link>
+    </div>
   )
 }
