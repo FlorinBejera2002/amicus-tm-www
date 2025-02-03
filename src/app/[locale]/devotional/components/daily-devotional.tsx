1da33a928d6433eefef3b33f3a861b232104501d
FlorinBejera2002 <florinpetru0306@gmail.com>
2025-02-03 13:03:16 +0200

    feat: add transition

--- src/app/[locale]/devotional/components/daily-devotional.tsx
+++ src/app/[locale]/devotional/components/daily-devotional.tsx
@@ -4,6 +4,7 @@ import { useEffect, useRef, useState } from 'react'
 import { FaDownload } from 'react-icons/fa6'
 import { toPng } from 'html-to-image'
 import { useTranslations } from 'next-intl'
+import InViewTransition from '../../common/in-view-transition'
 
 interface Verse {
   text: string
@@ -80,70 +81,82 @@ export default function DailyDevotional({
           backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${bgImage})`
         }}
       >
-        <h3 className="text-4xl font-bold text-accent text-center">
-          {devotional.title}
-        </h3>
-        <p className="text-gray-400 text-center ">
-          {devotional.week} - {devotional.day}
-        </p>
-        <div className="w-full justify-center flex">
-          <div className="mt-4 border-1 border-gray-500 p-2 rounded-md italic w-fit">
-            <p className="font-semibold text-white">
-              "{devotional.verse.text}"
-            </p>
-            <p className="text-right text-gray-300">
-              {devotional.verse.reference}
-            </p>
-          </div>
-        </div>
-
-        <div className="mt-6 space-y-6">
-          {devotional.content.map((item, index) => (
-            <div key={index} className="text-white">
-              {item.section_title && (
-                <h4 className="text-lg font-semibold text-white">
-                  {item.section_title}
-                </h4>
-              )}
-              {item.paragraph && <p>{item.paragraph}</p>}
-              {item.list && (
-                <ul className="list-disc pl-6 space-y-2">
-                  {item.list.map((listItem, i) => (
-                    <li key={i}>
-                      <strong className="text-accent">
-                        {listItem.title}:{' '}
-                      </strong>{' '}
-                      {listItem.description}
-                    </li>
-                  ))}
-                </ul>
-              )}
+        <InViewTransition delay={0.25}>
+          <h3 className="text-4xl font-bold text-accent text-center">
+            {devotional.title}
+          </h3>
+          <p className="text-gray-400 text-center ">
+            {devotional.week} - {devotional.day}
+          </p>
+        </InViewTransition>
+        <InViewTransition delay={0.5}>
+          <div className="w-full justify-center flex">
+            <div className="mt-4 border-1 border-gray-500 p-2 rounded-md italic w-fit">
+              <p className="font-semibold text-white">
+                "{devotional.verse.text}"
+              </p>
+              <p className="text-right text-gray-300">
+                {devotional.verse.reference}
+              </p>
             </div>
-          ))}
-        </div>
+          </div>
+        </InViewTransition>
 
-        <div className="mt-6">
-          <h4 className="text-lg font-semibold text-white">
-            Întrebări de reflecție:
-          </h4>
-          <ul className="list-disc pl-6 mt-2 text-white space-y-2">
-            {devotional.reflection_questions.map((question, index) => (
-              <li key={index}>{question}</li>
+        <InViewTransition delay={0.75}>
+          <div className="mt-6 space-y-6">
+            {devotional.content.map((item, index) => (
+              <div key={index} className="text-white">
+                {item.section_title && (
+                  <h4 className="text-lg font-semibold text-white">
+                    {item.section_title}
+                  </h4>
+                )}
+                {item.paragraph && <p>{item.paragraph}</p>}
+                {item.list && (
+                  <ul className="list-disc pl-6 space-y-2">
+                    {item.list.map((listItem, i) => (
+                      <li key={i}>
+                        <strong className="text-accent">
+                          {listItem.title}:{' '}
+                        </strong>{' '}
+                        {listItem.description}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
             ))}
-          </ul>
-        </div>
+          </div>
+        </InViewTransition>
 
-        <p className="text-accent text-xl font-semibold mt-10 text-center mb-0">
-          {devotional.conclusion}
-        </p>
+        <InViewTransition delay={1}>
+          <div className="mt-6">
+            <h4 className="text-lg font-semibold text-white">
+              Întrebări de reflecție:
+            </h4>
+            <ul className="list-disc pl-6 mt-2 text-white space-y-2">
+              {devotional.reflection_questions.map((question, index) => (
+                <li key={index}>{question}</li>
+              ))}
+            </ul>
+          </div>
+        </InViewTransition>
 
-        <button
-          onClick={handleDownloadImage}
-          className="absolute right-0 mt-20 text-black rounded bg-accent px-3 py-2 text-xs font-semibold flex items-center gap-2"
-        >
-          {t('button.download')}
-          <FaDownload />
-        </button>
+        <InViewTransition delay={1.25}>
+          <p className="text-accent text-xl font-semibold mt-10 text-center mb-0">
+            {devotional.conclusion}
+          </p>
+        </InViewTransition>
+
+        <InViewTransition delay={1.5}>
+          <button
+            onClick={handleDownloadImage}
+            className="absolute right-0 mt-20 text-black rounded bg-accent px-3 py-2 text-xs font-semibold flex items-center gap-2"
+          >
+            {t('button.download')}
+            <FaDownload />
+          </button>
+        </InViewTransition>
       </div>
     </div>
   )
