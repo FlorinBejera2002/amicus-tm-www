0f4f98adbbda3630da48f364a82555ff90b15c3a
FlorinBejera2002 <florinpetru0306@gmail.com>
2025-02-05 17:01:26 +0200

    feat: make text and padding consistensy

--- src/app/[locale]/devotional/components/daily-devotional.tsx
+++ src/app/[locale]/devotional/components/daily-devotional.tsx
@@ -73,91 +73,88 @@ export default function DailyDevotional({
   }
 
   return (
-    <div>
-      <div
-        ref={ref}
-        className="max-w-6xl mx-auto p-6 shadow-md  bg-cover bg-center md:p-16 rounded-md"
-        style={{
-          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${bgImage})`
-        }}
-      >
-        <InViewTransition delay={0.25}>
-          <h3 className="text-4xl font-bold text-accent text-center">
-            {devotional.title}
-          </h3>
-          <p className="text-gray-400 text-center ">
-            {devotional.week} - {devotional.day}
+    <div
+      ref={ref}
+      className="max-w-6xl mx-auto p-8 md:p-20 shadow-md bg-cover bg-center rounded-md"
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.6)), url(${bgImage})`
+      }}
+    >
+      <InViewTransition delay={0.25}>
+        <h3 className="text-xl md:text-4xl font-bold text-white">
+          {devotional.title}
+        </h3>
+        <p className="text-md text-gray-100">
+          {devotional.week} - {devotional.day}
+        </p>
+      </InViewTransition>
+
+      <InViewTransition delay={0.5}>
+        <div className="mt-8 border-1 border-gray-500 p-4 rounded-md italic w-fit">
+          <p className="font-semibold text-white">"{devotional.verse.text}"</p>
+          <p className="text-right text-gray-100">
+            {devotional.verse.reference}
           </p>
-        </InViewTransition>
-        <InViewTransition delay={0.5}>
-          <div className="w-full justify-center flex">
-            <div className="mt-4 border-1 border-gray-500 p-2 rounded-md italic w-fit">
-              <p className="font-semibold text-white">
-                "{devotional.verse.text}"
-              </p>
-              <p className="text-right text-gray-300">
-                {devotional.verse.reference}
-              </p>
+        </div>
+      </InViewTransition>
+
+      <InViewTransition delay={0.75}>
+        <div className="mt-8 space-y-6">
+          {devotional.content.map((item, index) => (
+            <div key={index}>
+              {item.section_title && (
+                <h4 className="text-xl font-semibold text-white">
+                  {item.section_title}
+                </h4>
+              )}
+              {item.paragraph && (
+                <p className="text-white text-lg">{item.paragraph}</p>
+              )}
+              {item.list && (
+                <ul className="list-disc space-y-2 text-gray-100 text-md">
+                  {item.list.map((listItem, i) => (
+                    <li key={i}>
+                      <strong className="text-lg text-white">
+                        {listItem.title}:
+                      </strong>{' '}
+                      {listItem.description}
+                    </li>
+                  ))}
+                </ul>
+              )}
             </div>
-          </div>
-        </InViewTransition>
+          ))}
+        </div>
+      </InViewTransition>
 
-        <InViewTransition delay={0.75}>
-          <div className="mt-6 space-y-6">
-            {devotional.content.map((item, index) => (
-              <div key={index} className="text-white">
-                {item.section_title && (
-                  <h4 className="text-lg font-semibold text-white">
-                    {item.section_title}
-                  </h4>
-                )}
-                {item.paragraph && <p>{item.paragraph}</p>}
-                {item.list && (
-                  <ul className="list-disc pl-6 space-y-2">
-                    {item.list.map((listItem, i) => (
-                      <li key={i}>
-                        <strong className="text-accent">
-                          {listItem.title}{' '}
-                        </strong>{' '}
-                        {listItem.description}
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </div>
+      <InViewTransition delay={1}>
+        <div className="mt-8">
+          <h4 className="text-lg font-semibold text-white">
+            {t('devotional.question')}
+          </h4>
+          <ul className="list-disc pl-6 mt-2 text-gray-100 space-y-2 text-md">
+            {devotional.reflection_questions.map((question, index) => (
+              <li key={index}>{question}</li>
             ))}
-          </div>
-        </InViewTransition>
+          </ul>
+        </div>
+      </InViewTransition>
 
-        <InViewTransition delay={1}>
-          <div className="mt-6">
-            <h4 className="text-lg font-semibold text-white">
-              Întrebări de reflecție:
-            </h4>
-            <ul className="list-disc pl-6 mt-2 text-white space-y-2">
-              {devotional.reflection_questions.map((question, index) => (
-                <li key={index}>{question}</li>
-              ))}
-            </ul>
-          </div>
-        </InViewTransition>
+      <InViewTransition delay={0.75}>
+        <p className="text-white text-lg font-semibold mt-10 mb-0">
+          {devotional.conclusion}
+        </p>
+      </InViewTransition>
 
-        <InViewTransition delay={1.25}>
-          <p className="text-accent text-xl font-semibold mt-10 text-center mb-0">
-            {devotional.conclusion}
-          </p>
-        </InViewTransition>
-
-        <InViewTransition delay={1.5}>
-          <button
-            onClick={handleDownloadImage}
-            className="absolute right-0  md:-right-16 mt-10 md:mt-20 text-black rounded-md bg-accent px-3 py-2 text-xs font-semibold flex items-center gap-2"
-          >
-            {t('button.download')}
-            <FaDownload />
-          </button>
-        </InViewTransition>
-      </div>
+      <InViewTransition delay={0.75}>
+        <button
+          onClick={handleDownloadImage}
+          className="absolute right-0 md:-right-20 mt-10 md:mt-24 text-black rounded-md bg-accent px-4 py-2 text-sm font-semibold flex items-center gap-2"
+        >
+          {t('button.download')}
+          <FaDownload />
+        </button>
+      </InViewTransition>
     </div>
   )
 }
