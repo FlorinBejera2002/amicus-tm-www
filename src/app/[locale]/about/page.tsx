a0dcd937b7bf2f5622ccd2a993625d23d6e60d3a
FlorinBejera2002 <florinpetru0306@gmail.com>
2025-02-06 15:12:45 +0200

    feat: improve about us page

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,11 +1,12 @@
 import InViewTransition from '../common/in-view-transition'
 import AccordionSection from './components/accordion-section'
 import ContactUsBtn from './components/contact-us-button'
-
+import Image from 'next/image'
 import type { Metadata } from 'next'
 import { useTranslations } from 'next-intl'
+import AboutImage from '../../../../public/About us page-bro.svg'
+import { FaChevronRight } from 'react-icons/fa'
 
-// eslint-disable-next-line react-refresh/only-export-components
 export const metadata: Metadata = {
   title: 'Arise for Christ | About'
 }
@@ -14,34 +15,51 @@ const About = () => {
   const t = useTranslations()
 
   return (
-    <div className="main flex flex-col items-center" role="main">
-      <div className="custom-page-content z-index-2 max-w-6xl">
-        <div className="rounded-md bg-white !p-8 shadow-lg md:!p-12">
-          <div className="row">
-            <div className="col">
-              <InViewTransition delay={0}>
-                <p className="text-md line-height-7 md:text-lg">
-                  {t('about_us.description_p1_1')}
+    <div className="flex flex-col items-center w-full">
+      <div className="w-full bg-gray-950 text-white pt-40 pb-12  flex flex-col items-center">
+        <h1 className="text-sm md:text-xl font-extrabold flex items-center text-accent">
+          {t('header.home')}
+          <FaChevronRight className="mx-2  h-4 w-4" />
+          <span className="">{t('header.about_us')}</span>
+        </h1>
+
+        <p className="mt-4 text-4xl mx-auto font-semibold">
+          {t('header.about_us')}
+        </p>
+      </div>
+
+      <div className="bg-gray-100 w-full">
+        <div className=" mx-auto max-w-6xl py-8 md:py-12 ">
+          <InViewTransition delay={0}>
+            <p className="text-lg text-gray-700">
+              {t('about_us.description_p1_1')}
+            </p>
+          </InViewTransition>
+
+          <div className="flex flex-col md:flex-row items-center">
+            <div className="md:w-1/2 flex flex-col gap-4">
+              <InViewTransition delay={0.5}>
+                <p className="text-lg text-gray-700">
+                  {t('about_us.description_p1_6')}
                 </p>
               </InViewTransition>
-            </div>
-          </div>
-          <div className="flex flex-col">
-            <InViewTransition delay={0.5}>
-              <p className="text-md line-height-7 md:text-lg max-w-[700px]">
-                {t('about_us.description_p1_6')}
-              </p>
-            </InViewTransition>
 
-            <AccordionSection />
-
-            <div className="appear-animation flex items-center py-4">
+              <AccordionSection />
               <ContactUsBtn />
+            </div>
+
+            <div className="md:w-1/2 flex justify-center">
+              <Image
+                src={AboutImage}
+                alt="About Illustration"
+                width={400}
+                className="w-full max-w-sm"
+              />
             </div>
           </div>
 
           <InViewTransition delay={0.25}>
-            <p className="text-md mb-0 pb-0 md:text-lg">
+            <p className="text-lg text-gray-700">
               {t('about_us.description_p3_1')}
             </p>
           </InViewTransition>
